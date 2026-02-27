1dda5a3eb708d7f0f259d8e2ee0a79929f00b31d
Rohit Muneshwar <rohit.muneshwar1406@gmail.com>
2026-02-27 14:39:58 +0100

    SpringAI: Audio to text

--- src/main/java/me/ai/training/config/AiConfig.java
+++ src/main/java/me/ai/training/config/AiConfig.java
@@ -1,8 +1,11 @@
 package me.ai.training.config;
 
 import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
+import org.springframework.ai.openai.api.OpenAiAudioApi;
+import org.springframework.ai.openai.api.common.OpenAiApiClientErrorException;
 import org.springframework.ai.vectorstore.SearchRequest;
 import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -18,6 +21,17 @@ public class AiConfig {
     QuestionAnswerAdvisor qaAdvisor(VectorStore vectorStore){
         return QuestionAnswerAdvisor.builder(vectorStore)
                 .searchRequest(SearchRequest.builder().topK(5).similarityThreshold(0.3).build())
+                .build();
+    }
+
+    @Bean
+    OpenAiAudioApi openAiAudioApi(@Value("${spring.ai.openai.audio.transcription.base-url}")
+                                  String endpoint,
+                                  @Value("${spring.ai.openai.api-key}")
+                                  String apiKey){
+        return new OpenAiAudioApi.Builder()
+                .baseUrl(endpoint)
+                .apiKey(apiKey)
                 .build();
     }
 }
---
+++ src/main/java/me/ai/training/controller/AudioController.java
@@ -0,0 +1,52 @@
+package me.ai.training.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import me.ai.training.service.AdvancedRagProcessingService;
+import me.ai.training.service.AudioService;
+import me.ai.training.service.FileProcessingRagService;
+import me.ai.training.service.RAGService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+
+/**
+ * @author Rohit Muneshwar
+ * @created on 2/27/2026
+ *
+ *
+ */
+@Tag(name = "Lab8: Audio", description = """
+        Audio to text with SpringAI
+        1. Conversion of text into audio
+        2. Converting audio to text (speech to text/Transcription)
+        3. Generating images
+        
+        Transcription
+          - to convert audio file to text in spring ai like whisper, gpt-4o-transcribe-gpt-4o-mini-transcribe
+        """)
+@RestController
+@RequestMapping("/audio")
+public class AudioController {
+    private final AudioService audioService;
+    public AudioController(AudioService audioService){
+        this.audioService = audioService;
+    }
+
+    @Operation(summary = "Audio To Text", description = """
+            Sample audio is present in the resources/audio folder. This examples uses manual OpneAiAudioApi config.
+            """)
+    @GetMapping("/v1/transcribe")
+    public String getAnswerUsingManualSettings(@RequestParam(name = "filename", defaultValue = "java.m4a") String fileName){
+        return audioService.getAnswerWithManualConfig(fileName);
+    }
+
+    @Operation(summary = "Audio To Text", description = """
+            Sample audio is present in the resources/audio folder.
+            """)
+    @GetMapping("/v2/transcribe")
+    public String getAnswer(@RequestParam(name = "filename", defaultValue = "java.m4a") String fileName){
+        return audioService.getAnswer(fileName);
+    }
+}
